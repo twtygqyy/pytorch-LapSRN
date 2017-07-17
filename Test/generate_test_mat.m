c0a699a121416eff47f1069497cd9ab7fa6c08b8
Jiu XU <twtygqyy@gmail.com>
2017-07-17 22:50:11 +0900

    fix mistake

--- Test/generate_test_mat.m
+++ Test/generate_test_mat.m
@@ -1,18 +1,19 @@
 clear;close all;
 %% settings
 folder = 'Set5';
+scale = 4;
 
 %% generate data
 filepaths = dir(fullfile(folder,'*.bmp'));
 
 for i = 1 : length(filepaths)        
     im_gt = imread(fullfile(folder,filepaths(i).name));
-    im_gt = modcrop(im_gt, 4);
+    im_gt = modcrop(im_gt, scale);
     im_gt = double(im_gt);
     im_gt_ycbcr = rgb2ycbcr(im_gt / 255.0);
     im_gt_y = im_gt_ycbcr(:,:,1) * 255.0;
-    im_l_ycbcr = imresize(im_gt_ycbcr,1/4,'bicubic');
-    im_b_ycbcr = imresize(im_l_ycbcr,4,'bicubic');
+    im_l_ycbcr = imresize(im_gt_ycbcr, 1/scale, 'bicubic');
+    im_b_ycbcr = imresize(im_l_ycbcr, scale, 'bicubic');
     im_l_y = im_l_ycbcr(:,:,1) * 255.0;
     im_l = ycbcr2rgb(im_l_ycbcr) * 255.0;
     im_b_y = im_b_ycbcr(:,:,1) * 255.0;
