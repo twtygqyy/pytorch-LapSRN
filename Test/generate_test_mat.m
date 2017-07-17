9c5c89b150f9ad27a608dd0c26f4c05fc9f5b60a
Jiu XU <twtygqyy@gmail.com>
2017-07-17 22:46:13 +0900

    code for generating test mat

---
+++ Test/generate_test_mat.m
@@ -0,0 +1,23 @@
+clear;close all;
+%% settings
+folder = 'Set5';
+
+%% generate data
+filepaths = dir(fullfile(folder,'*.bmp'));
+
+for i = 1 : length(filepaths)        
+    im_gt = imread(fullfile(folder,filepaths(i).name));
+    im_gt = modcrop(im_gt, 4);
+    im_gt = double(im_gt);
+    im_gt_ycbcr = rgb2ycbcr(im_gt / 255.0);
+    im_gt_y = im_gt_ycbcr(:,:,1) * 255.0;
+    im_l_ycbcr = imresize(im_gt_ycbcr,1/4,'bicubic');
+    im_b_ycbcr = imresize(im_l_ycbcr,4,'bicubic');
+    im_l_y = im_l_ycbcr(:,:,1) * 255.0;
+    im_l = ycbcr2rgb(im_l_ycbcr) * 255.0;
+    im_b_y = im_b_ycbcr(:,:,1) * 255.0;
+    im_b = ycbcr2rgb(im_b_ycbcr) * 255.0;
+
+    filename = sprintf('Set5-output/%s.mat',filepaths(i).name);
+    save(filename, 'im_gt_y', 'im_b_y', 'im_gt', 'im_b', 'im_l_ycbcr', 'im_l_y', 'im_l');
+end
