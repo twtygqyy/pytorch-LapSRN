3016f47b7272f960fcb53b81a50a36800807dab8
Jiu XU <twtygqyy@gmail.com>
2018-02-24 21:34:46 -0500

    update

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def main():
 
     global opt, model 
     opt = parser.parse_args()
-    print opt
+    print(opt)
 
     cuda = opt.cuda
     if cuda and not torch.cuda.is_available():
@@ -40,7 +40,7 @@ def main():
         torch.cuda.manual_seed(opt.seed)
 
     cudnn.benchmark = True
-        
+
     print("===> Loading datasets")
     train_set = DatasetFromHdf5("data/lap_pry_x4_small.h5")
     training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, batch_size=opt.batchSize, shuffle=True)
@@ -65,7 +65,7 @@ def main():
             model.load_state_dict(checkpoint["model"].state_dict())
         else:
             print("=> no checkpoint found at '{}'".format(opt.resume))
-            
+
     # optionally copy weights from a checkpoint
     if opt.pretrained:
         if os.path.isfile(opt.pretrained):
@@ -74,7 +74,7 @@ def main():
             model.load_state_dict(weights['model'].state_dict())
         else:
             print("=> no model found at '{}'".format(opt.pretrained)) 
-            
+
     print("===> Setting Optimizer")
     optimizer = optim.Adam(model.parameters(), lr=opt.lr)
 
@@ -82,7 +82,7 @@ def main():
     for epoch in range(opt.start_epoch, opt.nEpochs + 1): 
         train(training_data_loader, optimizer, model, criterion, epoch)
         save_checkpoint(model, epoch)
-    
+
 def adjust_learning_rate(optimizer, epoch):
     """Sets the learning rate to the initial LR decayed by 10 every 10 epochs"""
     lr = opt.lr * (0.1 ** (epoch // opt.step))
@@ -91,11 +91,11 @@ def adjust_learning_rate(optimizer, epoch):
 def train(training_data_loader, optimizer, model, criterion, epoch):
 
     lr = adjust_learning_rate(optimizer, epoch-1)
-    
+
     for param_group in optimizer.param_groups:
         param_group["lr"] = lr  
     print "epoch =", epoch,"lr =",optimizer.param_groups[0]["lr"]
-    model.train()    
+    model.train()
 
     for iteration, batch in enumerate(training_data_loader, 1):
 
@@ -105,24 +105,24 @@ def train(training_data_loader, optimizer, model, criterion, epoch):
             input = input.cuda()
             label_x2 = label_x2.cuda()
             label_x4 = label_x4.cuda()
-        
+
         HR_2x, HR_4x = model(input)
-        
+
         loss_x2 = criterion(HR_2x, label_x2)
         loss_x4 = criterion(HR_4x, label_x4)
         loss = loss_x2 + loss_x4
-                
+
         optimizer.zero_grad()
-        
-        loss_x2.backward(retain_variables=True)
-        
+
+        loss_x2.backward(retain_graph=True)
+
         loss_x4.backward()
 
         optimizer.step()
-        
+
         if iteration%100 == 0:
             print("===> Epoch[{}]({}/{}): Loss: {:.10f}".format(epoch, iteration, len(training_data_loader), loss.data[0]))
-    
+
 def save_checkpoint(model, epoch):
     model_folder = "model_adam/"
     model_out_path = model_folder + "model_epoch_{}.pth".format(epoch)
@@ -131,7 +131,7 @@ def save_checkpoint(model, epoch):
         os.makedirs(model_folder)
 
     torch.save(state, model_out_path)
-        
+
     print("Checkpoint saved to {}".format(model_out_path))
 
 if __name__ == "__main__":
